ec101471f497766fa8b3f879fbfee4b938ba3fe4
Fun7777777 <fun777777@163.com>
2026-02-23 22:16:25 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,13 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    pvcfsum = 0
+    dpvcfsum = 0
+    cf = couponRate * face
+    for t in range(1, (ppy*m)+1):
+        pv = (1 + y/ppy)**(-t)
+        pvcf = pv*cf/ppy
+        pvcfsum += pvcf
+        dpvcfsum += t*pvcf
+    bondprice = pvcfsum + pv*face
+    dbondprice = dpvcfsum + m*pv*face
+    return(dbondprice/bondprice)

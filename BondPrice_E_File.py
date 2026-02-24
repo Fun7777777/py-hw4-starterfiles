89306c8d2882702f6b1260aff3bb3f32804ccd5b
Fun7777777 <fun777777@163.com>
2026-02-23 23:05:39 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,16 @@
-
-
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    bondPrice = 0.0
+    coupon = face * couponRate
+    m = len(yc)
+    for idx, y in enumerate(yc):
+        t = idx + 1
+
+        if t < m:
+            cf = coupon
+        else:
+            cf = coupon + face
+
+        pv = 1 / (1 + y) ** t
+        bondPrice += cf * pv
+
+    return bondPrice

2bd9b96367c3d19fce4d11844e1497107fa5778e
Fun7777777 <fun777777@163.com>
2026-02-23 22:20:42 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,11 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    pvcfsum = 0
+    cf = couponRate * face
+    for timesZ, ycZ in zip(times,yc):
+        pv = (1 + ycZ)**-timesZ
+        pvcf = pv*cf
+        pvcfsum += pvcf
+    bondprice = pvcfsum + pv*face
+    return(bondprice)
